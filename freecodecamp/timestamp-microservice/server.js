33a5b23bcfd7c25742c0d04c98fdee8a1b968dfd
Stephania Nevado <stephania.arantxa@gmail.com>
2021-09-30 18:13:16 +0200

    Back End Development and APIs Projects: timestamp microservice

---
+++ freecodecamp/timestamp-microservice/server.js
@@ -0,0 +1,47 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/:api", (req, res) => {
+
+  let api = req.params.api;
+
+  if (api.match(/\d{5,}/)){
+    api = +api;
+  }
+
+  let date = new Date(api);
+  if(date.toUTCString() == "Invalid Date"){
+    res.json({error: date.toUTCString()})
+  }
+  res.json({unix: date.valueOf(), utc: date.toUTCString()});
+});
+
+app.get("/api", (req, res) => {
+  let date = new Date();
+  res.json({unix: date.valueOf(), utc: date.toUTCString()});
+})
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
